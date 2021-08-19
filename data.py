7b5a74355518330c7e3343109fb2da6b05a489e8
Hoang Cao Bao <hoangcaobao@MacOnes-MacBook-Pro.local>
2021-08-19 19:30:23 +0700

    update regex remove emoji

--- data.py
+++ data.py
@@ -27,10 +27,22 @@ class Dataset:
         #Remove url link in texts
         clean_text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
         return clean_text
-
+    def remove_emoji(self, data):
+        #Remove emoji tag link in texts
+        cleanr= re.compile("["
+                           u"\U0001F600-\U0001F64F"  
+                           u"\U0001F300-\U0001F5FF"
+                           u"\U0001F680-\U0001F6FF"  
+                           u"\U0001F1E0-\U0001F1FF"  
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+        clean_text=re.sub(cleanr, '',data)
+        return clean_text
+    
     def preprocess_data(self, text): 
         #Use all regex function we create above to clean data
-        processors = [self.remove_punc, self.remove_html, self.remove_urls]
+        processors = [self.remove_punc, self.remove_html, self.remove_urls, self.remove_emoji]
         for process in processors:
             text = process(text)
         return text
@@ -76,7 +88,7 @@ class Dataset:
                 All data has been cleaned using regex, token, and pad to same length
         """
         dataset, label_dataset = self.load_dataset(data_path, text_column, label_column)
-        
+     
         # split data 
         size = int(len(dataset) * (1 - self.test_size)) 
         self.x_train = dataset[:size]
