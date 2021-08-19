0f0624cc4bcd2187579bde872966dcea7b464b77
superstarlol999 <34463182+superstarlol999@users.noreply.github.com>
2021-08-20 03:24:24 +0700

    update draft-model1

--- model.py
+++ model.py
@@ -1,51 +1,71 @@
-import numpy as np
-import tensorflow as tf
 from tensorflow.keras.layers import *
-from tensorflow.python.ops.gen_math_ops import mod 
+import tensorflow as tf
 
 class CharCNN(tf.keras.models.Model):
-    def __init__(self,vocab_szie = 10000, embedding_size = 100, max_length = 2000, num_classes = 2,feature = 'small'):
+    def __init__(self,vocab_size, embedding_size, max_length, num_classes, feature = 'small', padding = 'valid'):
         super(CharCNN,self).__init__()
+        assert feature in ['small','large']
+        assert padding in ['valid','same']
+        self.padding = padding 
         self.num_classes = num_classes
 
-        assert feature in ['small','large']
         if feature == 'small':
-            self.units_fc = 1024
+            self.units_fc = 1024 #  The number of output units of fully-connected layer
             self.num_filter = 256
-            self.stddev = 0.05
+            self.stddev = 0.05 # standard deviation
         else:
-            self.units_fc = 2048
+            self.units_fc = 2048 # The number of output units of fully-connected layer
             self.num_filter = 1024
-            self.stddev = 0.02
-        self.vocab_size = vocab_szie
-        self.embedding_size = embedding_size
-        self.max_length = max_length
-    def call(self, data):
-        x = Embedding(input_dim=self.vocab_size, output_dim=self.embedding_size)(data) # embedding layer
+            self.stddev = 0.02 # standard deviation
+
+        # initialize the weights using a Gaussian distribution for conv layer
+        # The mean and standard deviation used for initializing the large model is (0, 0.02) and small model (0, 0.05).
+        self.initializers = tf.keras.initializers.RandomNormal(mean = 0., stddev= self.stddev, seed = 42)
+
+        # defind vocab_size,embedding_size,max_length
+        self.vocab_size, self.embedding_size, self.max_length = vocab_size,embedding_size,max_length
+
+        self.embedding = Embedding(self.vocab_size, self.embedding_size,input_length= self.max_length)
         # block Convolutional layers
-        #layer 1
-        x = Conv1D(filters= self.num_filter, kernel_size= 7,kernel_initializer= tf.keras.initializers.RandomNormal(mean = 0., stddev= self.stddev, seed= 2021), activation="relu", padding = 'same')(x)
-        x = MaxPooling1D(pool_size= 3)(x)
-        #layer 2
-        x = Conv1D(filters= self.num_filter, kernel_size= 7,kernel_initializer= tf.keras.initializers.RandomNormal(mean = 0., stddev= self.stddev, seed= 2021), activation="relu", padding = 'same')(x)
-        x = MaxPooling1D(pool_size= 3)(x)
-        #layer 3
-        x = Conv1D(filters= self.num_filter, kernel_size= 3,kernel_initializer= tf.keras.initializers.RandomNormal(mean = 0., stddev= self.stddev, seed= 2021), activation="relu", padding = 'same')(x)
-        #layer 4
-        x = Conv1D(filters= self.num_filter, kernel_size= 3,kernel_initializer= tf.keras.initializers.RandomNormal(mean = 0., stddev= self.stddev, seed= 2021), activation="relu", padding = 'same')(x)
-        #layer 5 
-        x = Conv1D(filters= self.num_filter, kernel_size= 3,kernel_initializer= tf.keras.initializers.RandomNormal(mean = 0., stddev= self.stddev, seed= 2021), activation="relu", padding = 'same')(x)
-        #layer 6 
-        x = Conv1D(filters= self.num_filter, kernel_size= 3,kernel_initializer= tf.keras.initializers.RandomNormal(mean = 0., stddev= self.stddev, seed= 2021), activation="relu", padding = 'same')(x)
-        x = MaxPooling1D(pool_size= 3)(x)
-        # end Convolutional layers
-        x = Flatten()(x) # flatten layer
-        # block Full-Connected layers
-        x = Dense(self.units_fc,activation= 'relu')(x)
-        x = Dropout(0.5)(x) # Droppout layer 1
-        x = Dense(self.units_fc,activation= 'relu')(x)
-        x = Dropout(0.5)(x)  # Droppout layer 2
-        x = Dense(units= self.num_classes, activation= 'softmax')(x)
+        # block conv layer 1
+        self.conv1d_1 = Conv1D(self.num_filter, kernel_size= 7, kernel_initializer= self.initializers, activation= 'relu',padding = self.padding)
+        self.maxpooling1d_1 = MaxPooling1D(3)
+        # block conv layer 2
+        self.conv1d_2 = Conv1D(self.num_filter, kernel_size= 7, kernel_initializer= self.initializers, activation= 'relu',padding = self.padding)
+        self.maxpooling1d_2 = MaxPooling1D(3)
+        # block conv layer 3
+        self.conv1d_3 = Conv1D(self.num_filter, kernel_size= 3, kernel_initializer= self.initializers, activation= 'relu',padding = self.padding)
+        # block conv layer 4
+        self.conv1d_4 = Conv1D(self.num_filter, kernel_size= 3, kernel_initializer= self.initializers, activation= 'relu',padding = self.padding)
+        # block conv layer 5
+        self.conv1d_5 = Conv1D(self.num_filter, kernel_size= 3, kernel_initializer= self.initializers, activation= 'relu',padding = self.padding)
+        # block conv layer 6
+        self.conv1d_6 = Conv1D(self.num_filter, kernel_size= 3, kernel_initializer= self.initializers, activation= 'relu',padding = self.padding)
+        self.maxpooling1d_6 = MaxPooling1D(3)
+        
+        self.flatten = Flatten() # flatten layer
+
+        # block Fully-connected layers
+        self.fc1 = Dense(self.units_fc,activation= 'relu')
+        self.drp1 = Dropout(0.5)
+        self.fc2 = Dense(self.units_fc,activation= 'relu')
+        self.drp2 = Dropout(0.5)
+        self.fc3 = Dense(self.num_classes, activation= 'softmax')
+        
+    def call(self,data):
+        x = self.embedding(data)
+        x = self.maxpooling1d_1(self.conv1d_1(x))
+        x = self.maxpooling1d_2(self.conv1d_2(x))
+        x = self.conv1d_3(x)
+        x = self.conv1d_4(x)
+        x = self.conv1d_5(x)
+        x = self.maxpooling1d_6(self.conv1d_6(x))
+
+        x = self.flatten(x)
+         # linear layer 
+        x = self.drp1(self.fc1(x))
+        # linear layer 
+        x = self.drp2(self.fc2(x))
+        # ouput layer
+        x = self.fc3(x)
         return x
-        # end 
-      
