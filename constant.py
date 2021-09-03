a0764666e45071b5cd734400853ee0879670b9a2
Hoang Cao Bao <hoangcaobao@MacOnes-MacBook-Pro.local>
2021-09-03 08:01:22 -0400

    merge

--- constant.py
+++ constant.py
@@ -2,10 +2,7 @@
 padding = "post"
 label = "positive"
 oov_token="<OOV>"
-<<<<<<< HEAD
 metric="accuracy"
 data_path = "IMDB Dataset.csv"
 text_column = "review"
 label_column = "sentiment"
-=======
->>>>>>> main
